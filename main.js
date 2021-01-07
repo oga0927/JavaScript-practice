b818bbe5e48986d91327734877c103f228e7c411
oga <totti0927.oga@gmail.com>
2021-01-07 23:36:13 +0900

    filtered

--- main.js
+++ main.js
@@ -10,13 +10,29 @@
 // prices;
 // console.log(prices);
 
-function handlePosts() {
-  var posts = [
-    { id: 23, title: 'JSニュース' },
-    { id: 52, title: 'リファクター・シティ' },
-    { id: 105, title: 'Rubyの良いところ' }
-  ];
-  posts.forEach(function(post) {
-    savePost(post);
-  });
+// function handlePosts() {
+//   var posts = [
+//     { id: 23, title: 'JSニュース' },
+//     { id: 52, title: 'リファクター・シティ' },
+//     { id: 105, title: 'Rubyの良いところ' }
+//   ];
+//   posts.forEach(function(post) {
+//     savePost(post);
+//   });
+// }
+
+var products = [
+  { name: 'きゅうり', type: '野菜'},
+  { name: 'バナナ', type: 'フルーツ'},
+  { name: 'セロリ', type: '野菜'},
+  { name: 'オレンジ', type: 'フルーツ'}
+];
+
+var filteredProducdts = [];
+
+for (var i = 0; i < products.length; i++) {
+  if (products[i].type === 'フルーツ') {
+    filteredProducdts.push(products[i]);
+    console.log(filteredProducdts);
+  }
 }
