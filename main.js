d719cf824df16d361a45e2dda2f3a92bacd2e4a0
oga <totti0927.oga@gmail.com>
2021-01-08 23:48:46 +0900

    応用編

--- main.js
+++ main.js
@@ -22,17 +22,24 @@
 // }
 
 var products = [
-  { name: 'きゅうり', type: '野菜'},
-  { name: 'バナナ', type: 'フルーツ'},
-  { name: 'セロリ', type: '野菜'},
-  { name: 'オレンジ', type: 'フルーツ'}
+  { name: 'きゅうり', type: '野菜', quantity: 0,price: 1},
+  { name: 'バナナ', type: 'フルーツ', quantity: 10,price: 15},
+  { name: 'セロリ', type: '野菜', quantity: 30, price: 9},
+  { name: 'オレンジ', type: 'フルーツ', quantity: 3, price: 5}
 ];
 
 var filteredProducdts = [];
 
-for (var i = 0; i < products.length; i++) {
-  if (products[i].type === 'フルーツ') {
-    filteredProducdts.push(products[i]);
-    console.log(filteredProducdts);
-  }
-}
+// for (var i = 0; i < products.length; i++) {
+//   if (products[i].type === 'フルーツ') {
+//     filteredProducdts.push(products[i]);
+//     console.log(filteredProducdts);
+//   } 
+// }
+
+products.filter(function(product) {
+  return product.type === '野菜'　
+  && product.quantity > 0 
+  && product.price < 10;
+});
+console.log(filteredProducdts);
