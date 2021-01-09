9737aff435f8903dd4de635206f3c7541918552c
oga <totti0927.oga@gmail.com>
2021-01-09 23:55:37 +0900

    filter

--- main.js
+++ main.js
@@ -21,14 +21,14 @@
 //   });
 // }
 
-var products = [
-  { name: 'きゅうり', type: '野菜', quantity: 0,price: 1},
-  { name: 'バナナ', type: 'フルーツ', quantity: 10,price: 15},
-  { name: 'セロリ', type: '野菜', quantity: 30, price: 9},
-  { name: 'オレンジ', type: 'フルーツ', quantity: 3, price: 5}
-];
+// var products = [
+//   { name: 'きゅうり', type: '野菜', quantity: 0,price: 1},
+//   { name: 'バナナ', type: 'フルーツ', quantity: 10,price: 15},
+//   { name: 'セロリ', type: '野菜', quantity: 30, price: 9},
+//   { name: 'オレンジ', type: 'フルーツ', quantity: 3, price: 5}
+// ];
 
-var filteredProducdts = [];
+// var filteredProducdts = [];
 
 // for (var i = 0; i < products.length; i++) {
 //   if (products[i].type === 'フルーツ') {
@@ -37,9 +37,27 @@ var filteredProducdts = [];
 //   } 
 // }
 
-products.filter(function(product) {
-  return product.type === '野菜'　
-  && product.quantity > 0 
-  && product.price < 10;
-});
-console.log(filteredProducdts);
+// products.filter(function(product) {
+//   return product.type === '野菜'　
+//   && product.quantity > 0 
+//   && product.price < 10;
+// });
+// console.log(filteredProducdts);
+
+var post = {id: 4,title:'初めての投稿'};
+var comments = [
+  {postId: 4, content:'いい記事ですね'},
+  {postId: 3, content:'勉強になりました'},
+  {postId: 4, content:'なるほど'}
+];
+
+function commentsForPost(post, comments) {
+  return comments.filter(function(comment) {
+    return comment.postId === post.id;
+  });
+}
+
+commentsForPost(post, comments);
+
+console.log(commentsForPost(post,comments));
+
