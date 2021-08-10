005f465ac0f79f5331ef87cbe0a3d66a08a32b47
OGA <totti0927.oga@gmail.com>
2021-08-10 09:49:30 +0900

    mapメソッド

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ var areas = [];
 images.forEach((image) => {
   areas.push(image.height * images.width)
 });
+
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights = images.map((image) => {
+  return image.height;
+});
+
+
+// 距離と速度を求める
+// var trips = [
+//   { distance: 34, time: 10 },
+//   { distance: 90, time: 50 },
+//   { distance: 59, time: 25 }
+// ];
+
+// var speeds;
+
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map((trip) => {
+    return trip.distance / trip.time;
+});
