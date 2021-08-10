433705d2e10665b5d7618f2de891354d6f602501
OGA <totti0927.oga@gmail.com>
2021-08-10 09:23:07 +0900

    forEach

---
+++ main.js
@@ -0,0 +1,10 @@
+var images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+var areas = [];
+
+images.forEach((image) => {
+  areas.push(image.height * images.width)
+});
