31cb22b9ddd64ba6013d2ec32fc72c2cf7c9af03
OGA <totti0927.oga@gmail.com>
2021-08-22 09:46:29 +0900

    型宣言

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-function fn() {
-  if(true) {
-    let a = 'fn called';
-    return a;
-  }
-}
+// function fn() {
+//   if(true) {
+//     let a = 'fn called';
+//     return a;
+//   }
+// }
 
-const result = fn();
-console.log(result);
+// const result = fn();
+// console.log(result);
+
+// オブジェクトについて
+// const character = {
+//   name: 'micky',
+//   kinds: 'mouse',
+// }
+
+var users = [
+  { id: 1, admin: false },
+  { id: 2, admin: false },
+  { id: 3, admin: true }
+];
+
+var admin = users.find((user) => {
+    return user.admin === true;
+})
+admin;
---
+++ 暗黙的な型変換/main.js
@@ -0,0 +1,29 @@
+function printTypeAndValue(val) {
+  console.log(typeof val, val);
+}
+
+
+let a = 0;
+
+printTypeAndValue(a);
+
+
+
+// 文字列を数値に変換するときはparseInt
+let b = parseInt('1') + a;
+
+printTypeAndValue(b);
+
+// 15から文字列の10を計算した場合、- の演算子は数値の計算にしか
+// 使われないため、bの文字列は数値に変換される
+let c = 15 - b;
+
+printTypeAndValue(c);
+
+// nullは参照を保持していない　空を意味する
+let d = c - null;
+printTypeAndValue(d)
+
+// trueは数値の1に変換する
+let e = d - true;
+printTypeAndValue(e)
